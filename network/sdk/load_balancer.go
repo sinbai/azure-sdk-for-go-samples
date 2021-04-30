cfa04b4e689602be029b2eb8fa808160a204ec2b
elena <sinbai@hotmail.com>
2021-04-30 14:55:49 +0800

    add load balancer sdk test case

---
+++ network/sdk/load_balancer.go
@@ -0,0 +1,541 @@
+// Copyright (c) Microsoft and contributors.  All rights reserved.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+package network
+
+import (
+	"context"
+	"log"
+	"net/url"
+	"strings"
+	"time"
+
+	"github.com/Azure-Samples/azure-sdk-for-go-samples/internal/config"
+	"github.com/Azure/azure-sdk-for-go/sdk/arm/network/2020-07-01/armnetwork"
+	"github.com/Azure/azure-sdk-for-go/sdk/armcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/to"
+)
+
+var (
+	loadBalancingRuleName       string
+	outBoundRuleName            string
+	probeName                   string
+	frontendIpConfigurationName string
+	backendAddressPoolName      string
+	inboundNatruleName          string
+)
+
+func getLoadBalancersClient() armnetwork.LoadBalancersClient {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	client := armnetwork.NewLoadBalancersClient(armcore.NewDefaultConnection(cred, nil), config.SubscriptionID())
+	return *client
+}
+
+// Create LoadBalancers
+func CreateLoadBalancer(ctx context.Context, loadBalancerName string, publicIpAddressName string) error {
+	loadBalancingRuleName = config.AppendRandomSuffix("loadbalancingrule")
+	outBoundRuleName = config.AppendRandomSuffix("outboundrule")
+	probeName = config.AppendRandomSuffix("probe")
+	frontendIpConfigurationName = config.AppendRandomSuffix("frontendipconfiguration")
+	backendAddressPoolName = config.AppendRandomSuffix("backendaddresspool")
+	inboundNatruleName = config.AppendRandomSuffix("inboundnatrule")
+
+	urlPathAddress := "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Network/publicIPAddresses/{publicIpAddressName}"
+	urlPathAddress = strings.ReplaceAll(urlPathAddress, "{resourceGroupName}", url.PathEscape(config.GroupName()))
+	urlPathAddress = strings.ReplaceAll(urlPathAddress, "{publicIpAddressName}", url.PathEscape(publicIpAddressName))
+	urlPathAddress = strings.ReplaceAll(urlPathAddress, "{subscriptionId}", url.PathEscape(config.SubscriptionID()))
+
+	urlPathFrontendIPConfiguration := "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Network/loadBalancers/{loadBalancerName}/frontendIPConfigurations/{frontendIpConfigurationName}"
+	urlPathFrontendIPConfiguration = strings.ReplaceAll(urlPathFrontendIPConfiguration, "{resourceGroupName}", url.PathEscape(config.GroupName()))
+	urlPathFrontendIPConfiguration = strings.ReplaceAll(urlPathFrontendIPConfiguration, "{loadBalancerName}", url.PathEscape(loadBalancerName))
+	urlPathFrontendIPConfiguration = strings.ReplaceAll(urlPathFrontendIPConfiguration, "{subscriptionId}", url.PathEscape(config.SubscriptionID()))
+	urlPathFrontendIPConfiguration = strings.ReplaceAll(urlPathFrontendIPConfiguration, "{frontendIpConfigurationName}", url.PathEscape(frontendIpConfigurationName))
+
+	urlPathBackendAddressPool := "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Network/loadBalancers/{loadBalancerName}/backendAddressPools/{backendAddressPoolName}"
+	urlPathBackendAddressPool = strings.ReplaceAll(urlPathBackendAddressPool, "{resourceGroupName}", url.PathEscape(config.GroupName()))
+	urlPathBackendAddressPool = strings.ReplaceAll(urlPathBackendAddressPool, "{loadBalancerName}", url.PathEscape(loadBalancerName))
+	urlPathBackendAddressPool = strings.ReplaceAll(urlPathBackendAddressPool, "{subscriptionId}", url.PathEscape(config.SubscriptionID()))
+	urlPathBackendAddressPool = strings.ReplaceAll(urlPathBackendAddressPool, "{backendAddressPoolName}", url.PathEscape(backendAddressPoolName))
+
+	urlPathProb := "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Network/loadBalancers/{loadBalancerName}/probes/{probeName}"
+	urlPathProb = strings.ReplaceAll(urlPathProb, "{resourceGroupName}", url.PathEscape(config.GroupName()))
+	urlPathProb = strings.ReplaceAll(urlPathProb, "{loadBalancerName}", url.PathEscape(loadBalancerName))
+	urlPathProb = strings.ReplaceAll(urlPathProb, "{subscriptionId}", url.PathEscape(config.SubscriptionID()))
+	urlPathProb = strings.ReplaceAll(urlPathProb, "{probeName}", url.PathEscape(probeName))
+
+	client := getLoadBalancersClient()
+	poller, err := client.BeginCreateOrUpdate(
+		ctx,
+		config.GroupName(),
+		loadBalancerName,
+		armnetwork.LoadBalancer{
+			Resource: armnetwork.Resource{
+				Location: to.StringPtr(config.Location()),
+			},
+			Properties: &armnetwork.LoadBalancerPropertiesFormat{
+				BackendAddressPools: &[]armnetwork.BackendAddressPool{
+					{
+						Name: &backendAddressPoolName,
+					},
+				},
+				FrontendIPConfigurations: &[]armnetwork.FrontendIPConfiguration{
+					{
+						Name: &frontendIpConfigurationName,
+						Properties: &armnetwork.FrontendIPConfigurationPropertiesFormat{
+							PublicIPAddress: &armnetwork.PublicIPAddress{
+								Resource: armnetwork.Resource{
+									ID: &urlPathAddress,
+								},
+							},
+						},
+					},
+				},
+				LoadBalancingRules: &[]armnetwork.LoadBalancingRule{
+					{
+						Name: &loadBalancingRuleName,
+						Properties: &armnetwork.LoadBalancingRulePropertiesFormat{
+							BackendAddressPool: &armnetwork.SubResource{
+								ID: &urlPathBackendAddressPool,
+							},
+							BackendPort:         to.Int32Ptr(80),
+							DisableOutboundSnat: to.BoolPtr(true),
+							EnableFloatingIP:    to.BoolPtr(true),
+							EnableTCPReset:      new(bool),
+							FrontendIPConfiguration: &armnetwork.SubResource{
+								ID: &urlPathFrontendIPConfiguration,
+							},
+							FrontendPort:         to.Int32Ptr(80),
+							IDleTimeoutInMinutes: to.Int32Ptr(15),
+							LoadDistribution:     armnetwork.LoadDistributionDefault.ToPtr(),
+							Probe: &armnetwork.SubResource{
+								ID: &urlPathProb,
+							},
+							Protocol: armnetwork.TransportProtocolTCP.ToPtr(),
+						},
+					},
+				},
+				OutboundRules: &[]armnetwork.OutboundRule{
+					{
+						Name: &outBoundRuleName,
+						Properties: &armnetwork.OutboundRulePropertiesFormat{
+							BackendAddressPool: &armnetwork.SubResource{
+								ID: &urlPathBackendAddressPool,
+							},
+							FrontendIPConfigurations: &[]armnetwork.SubResource{
+								{
+									ID: &urlPathFrontendIPConfiguration,
+								},
+							},
+							Protocol: armnetwork.LoadBalancerOutboundRuleProtocolAll.ToPtr(),
+						},
+					},
+				},
+				Probes: &[]armnetwork.Probe{
+					{
+						Name: &probeName,
+						Properties: &armnetwork.ProbePropertiesFormat{
+							IntervalInSeconds: to.Int32Ptr(15),
+							NumberOfProbes:    to.Int32Ptr(2),
+							Port:              to.Int32Ptr(80),
+							Protocol:          armnetwork.ProbeProtocolHTTP.ToPtr(),
+							RequestPath:       to.StringPtr("healthcheck.aspx"),
+						},
+					},
+				},
+			},
+			SKU: &armnetwork.LoadBalancerSKU{
+				Name: armnetwork.LoadBalancerSKUNameStandard.ToPtr(),
+			},
+		},
+		nil,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	_, err = poller.PollUntilDone(ctx, 30*time.Second)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Gets the specified load balancer in a specified resource group.
+func GetLoadBalancer(ctx context.Context, loadBalancerName string) error {
+	client := getLoadBalancersClient()
+	_, err := client.Get(ctx, config.GroupName(), loadBalancerName, nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Gets all the load balancers in a resource group
+func ListLoadBalancer(ctx context.Context) error {
+	client := getLoadBalancersClient()
+	pager := client.List(config.GroupName(), nil)
+
+	for pager.NextPage(ctx) {
+		if pager.Err() != nil {
+			return pager.Err()
+		}
+	}
+
+	if pager.Err() != nil {
+		return pager.Err()
+	}
+	return nil
+}
+
+// Gets all the load balancer in a subscription.
+func ListAllLoadBalancer(ctx context.Context) error {
+	client := getLoadBalancersClient()
+	pager := client.ListAll(nil)
+	for pager.NextPage(ctx) {
+		if pager.Err() != nil {
+			return pager.Err()
+		}
+	}
+
+	if pager.Err() != nil {
+		return pager.Err()
+	}
+	return nil
+}
+
+// Updates a load balancer tags.
+func UpdateLoadBalancerTags(ctx context.Context, loadBalancerName string) error {
+	client := getLoadBalancersClient()
+	_, err := client.UpdateTags(
+		ctx,
+		config.GroupName(),
+		loadBalancerName,
+		armnetwork.TagsObject{
+			Tags: &map[string]string{"tag1": "value1", "tag2": "value2"},
+		},
+		nil,
+	)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Deletes the specified load balancer.
+func DeleteLoadBalancer(ctx context.Context, loadBalancerName string) error {
+	client := getLoadBalancersClient()
+	resp, err := client.BeginDelete(ctx, config.GroupName(), loadBalancerName, nil)
+	if err != nil {
+		return err
+	}
+	_, err = resp.PollUntilDone(ctx, 30*time.Second)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func getLoadBalancerFrontendIPConfigurationsClient() armnetwork.LoadBalancerFrontendIPConfigurationsClient {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	client := armnetwork.NewLoadBalancerFrontendIPConfigurationsClient(armcore.NewDefaultConnection(cred, nil), config.SubscriptionID())
+	return *client
+}
+
+// Gets load balancer frontend IP configuration.
+func GetLoadBalancerFrontendIPConfiguration(ctx context.Context, loadBalancerName string, loadBalancerFrontendIPConfigurationName string) error {
+	client := getLoadBalancerFrontendIPConfigurationsClient()
+	_, err := client.Get(ctx, config.GroupName(), loadBalancerName, loadBalancerFrontendIPConfigurationName, nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Gets all the load balancer frontend IP configurations.
+func ListLoadBalancerFrontendIPConfiguration(ctx context.Context, loadBalancerName string) error {
+	client := getLoadBalancerFrontendIPConfigurationsClient()
+	pager := client.List(config.GroupName(), loadBalancerName, nil)
+
+	for pager.NextPage(ctx) {
+		if pager.Err() != nil {
+			return pager.Err()
+		}
+	}
+
+	if pager.Err() != nil {
+		return pager.Err()
+	}
+	return nil
+}
+
+func getLoadBalancerBackendAddressPoolsClient() armnetwork.LoadBalancerBackendAddressPoolsClient {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	client := armnetwork.NewLoadBalancerBackendAddressPoolsClient(armcore.NewDefaultConnection(cred, nil), config.SubscriptionID())
+	return *client
+}
+
+// Gets load balancer backend address pool.
+func GetLoadBalancerBackendAddressPool(ctx context.Context, loadBalancerBackendAddressPoolName string, backendAddressPoolName string) error {
+	client := getLoadBalancerBackendAddressPoolsClient()
+	_, err := client.Get(ctx, config.GroupName(), loadBalancerBackendAddressPoolName, backendAddressPoolName, nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Gets all the load balancer backed address pools.
+func ListLoadBalancerBackendAddressPool(ctx context.Context, loadBalancerName string) error {
+	client := getLoadBalancerBackendAddressPoolsClient()
+	pager := client.List(config.GroupName(), loadBalancerName, nil)
+
+	for pager.NextPage(ctx) {
+		if pager.Err() != nil {
+			return pager.Err()
+		}
+	}
+
+	if pager.Err() != nil {
+		return pager.Err()
+	}
+	return nil
+}
+
+func getInboundNatRulesClient() armnetwork.InboundNatRulesClient {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	client := armnetwork.NewInboundNatRulesClient(armcore.NewDefaultConnection(cred, nil), config.SubscriptionID())
+	return *client
+}
+
+// Creates or updates a load balancer inbound nat rule.
+func CreateInboundNatRule(ctx context.Context, loadBalancerName string, inboundNatRuleName string) error {
+	urlPathFrontendIPConfiguration := "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Network/loadBalancers/{loadBalancerName}/frontendIPConfigurations/{frontendIpConfigurationName}"
+	urlPathFrontendIPConfiguration = strings.ReplaceAll(urlPathFrontendIPConfiguration, "{resourceGroupName}", url.PathEscape(config.GroupName()))
+	urlPathFrontendIPConfiguration = strings.ReplaceAll(urlPathFrontendIPConfiguration, "{loadBalancerName}", url.PathEscape(loadBalancerName))
+	urlPathFrontendIPConfiguration = strings.ReplaceAll(urlPathFrontendIPConfiguration, "{subscriptionId}", url.PathEscape(config.SubscriptionID()))
+	urlPathFrontendIPConfiguration = strings.ReplaceAll(urlPathFrontendIPConfiguration, "{frontendIpConfigurationName}", url.PathEscape(frontendIpConfigurationName))
+
+	client := getInboundNatRulesClient()
+	poller, err := client.BeginCreateOrUpdate(
+		ctx,
+		config.GroupName(),
+		loadBalancerName,
+		inboundNatRuleName,
+		armnetwork.InboundNatRule{
+			Properties: &armnetwork.InboundNatRulePropertiesFormat{
+				BackendIPConfiguration: &armnetwork.NetworkInterfaceIPConfiguration{},
+				BackendPort:            to.Int32Ptr(3389),
+				EnableFloatingIP:       to.BoolPtr(false),
+				EnableTCPReset:         to.BoolPtr(false),
+				FrontendIPConfiguration: &armnetwork.SubResource{
+					ID: &urlPathFrontendIPConfiguration,
+				},
+				FrontendPort:         to.Int32Ptr(3390),
+				IDleTimeoutInMinutes: to.Int32Ptr(4),
+				Protocol:             armnetwork.TransportProtocolTCP.ToPtr(),
+			},
+		},
+		nil,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	_, err = poller.PollUntilDone(ctx, 30*time.Second)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Gets the specified load balancer inbound nat rule.
+func GetInboundNatRule(ctx context.Context, loadBalancerName string, inboundNatRuleName string) error {
+	client := getInboundNatRulesClient()
+	_, err := client.Get(ctx, config.GroupName(), loadBalancerName, inboundNatRuleName, nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Gets all the inbound nat rules in a load balancer.
+func ListInboundNatRule(ctx context.Context, loadBalancerName string) error {
+	client := getInboundNatRulesClient()
+	pager := client.List(config.GroupName(), loadBalancerName, nil)
+
+	for pager.NextPage(ctx) {
+		if pager.Err() != nil {
+			return pager.Err()
+		}
+	}
+
+	if pager.Err() != nil {
+		return pager.Err()
+	}
+	return nil
+}
+
+// Deletes the specified load balancer inbound nat rule.
+func DeleteInboundNatRule(ctx context.Context, loadBalancerName string, inboundNatRuleName string) error {
+	client := getInboundNatRulesClient()
+	resp, err := client.BeginDelete(ctx, config.GroupName(), loadBalancerName, inboundNatRuleName, nil)
+	if err != nil {
+		return err
+	}
+	_, err = resp.PollUntilDone(ctx, 30*time.Second)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func getLoadBalancerLoadBalancingRulesClient() armnetwork.LoadBalancerLoadBalancingRulesClient {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	client := armnetwork.NewLoadBalancerLoadBalancingRulesClient(armcore.NewDefaultConnection(cred, nil), config.SubscriptionID())
+	return *client
+}
+
+// Gets the specified load balancer load balancing rule.
+func GetLoadBalancerLoadBalancingRule(ctx context.Context, loadBalancerName string, loadBalancingRuleName string) error {
+	client := getLoadBalancerLoadBalancingRulesClient()
+	_, err := client.Get(ctx, config.GroupName(), loadBalancerName, loadBalancingRuleName, nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Gets all the load balancing rules in a load balancer.
+func ListLoadBalancerLoadBalancingRule(ctx context.Context, loadBalancerName string) error {
+	client := getLoadBalancerLoadBalancingRulesClient()
+	pager := client.List(config.GroupName(), loadBalancerName, nil)
+
+	for pager.NextPage(ctx) {
+		if pager.Err() != nil {
+			return pager.Err()
+		}
+	}
+
+	if pager.Err() != nil {
+		return pager.Err()
+	}
+	return nil
+}
+
+func getLoadBalancerOutboundRulesClient() armnetwork.LoadBalancerOutboundRulesClient {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	client := armnetwork.NewLoadBalancerOutboundRulesClient(armcore.NewDefaultConnection(cred, nil), config.SubscriptionID())
+	return *client
+}
+
+// Gets the specified load balancer outbound rule.
+func GetLoadBalancerOutboundRule(ctx context.Context, loadBalancerName string, outBoundRuleName string) error {
+	client := getLoadBalancerOutboundRulesClient()
+	_, err := client.Get(ctx, config.GroupName(), loadBalancerName, outBoundRuleName, nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Gets all the outbound rules in a load balancer.
+func ListLoadBalancerOutboundRule(ctx context.Context, loadBalancerName string) error {
+	client := getLoadBalancerOutboundRulesClient()
+	pager := client.List(config.GroupName(), loadBalancerName, nil)
+
+	for pager.NextPage(ctx) {
+		if pager.Err() != nil {
+			return pager.Err()
+		}
+	}
+
+	if pager.Err() != nil {
+		return pager.Err()
+	}
+	return nil
+}
+
+func getLoadBalancerProbesClient() armnetwork.LoadBalancerProbesClient {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	client := armnetwork.NewLoadBalancerProbesClient(armcore.NewDefaultConnection(cred, nil), config.SubscriptionID())
+	return *client
+}
+
+// Gets load balancer probe.
+func GetLoadBalancerProbe(ctx context.Context, loadBalancerName string, probeName string) error {
+	client := getLoadBalancerProbesClient()
+	_, err := client.Get(ctx, config.GroupName(), loadBalancerName, probeName, nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Gets all the load balancer probes.
+func ListLoadBalancerProbe(ctx context.Context, loadBalancerName string) error {
+	client := getLoadBalancerProbesClient()
+	pager := client.List(config.GroupName(), loadBalancerName, nil)
+
+	for pager.NextPage(ctx) {
+		if pager.Err() != nil {
+			return pager.Err()
+		}
+	}
+
+	if pager.Err() != nil {
+		return pager.Err()
+	}
+	return nil
+}
+
+func getLoadBalancerNetworkInterfacesClient() armnetwork.LoadBalancerNetworkInterfacesClient {
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		log.Fatalf("failed to obtain a credential: %v", err)
+	}
+	client := armnetwork.NewLoadBalancerNetworkInterfacesClient(armcore.NewDefaultConnection(cred, nil), config.SubscriptionID())
+	return *client
+}
+
+// Gets associated load balancer network interfaces.
+func ListLoadBalancerNetworkInterface(ctx context.Context, loadBalancerName string) error {
+	client := getLoadBalancerNetworkInterfacesClient()
+	pager := client.List(config.GroupName(), loadBalancerName, nil)
+
+	for pager.NextPage(ctx) {
+		if pager.Err() != nil {
+			return pager.Err()
+		}
+	}
+
+	if pager.Err() != nil {
+		return pager.Err()
+	}
+	return nil
+}
---
+++ network/sdk/load_balancer_test.go
@@ -0,0 +1,182 @@
+// Copyright (c) Microsoft and contributors.  All rights reserved.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+package network
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/Azure-Samples/azure-sdk-for-go-samples/internal/config"
+	"github.com/Azure-Samples/azure-sdk-for-go-samples/resources"
+	"github.com/Azure/azure-sdk-for-go/sdk/arm/network/2020-07-01/armnetwork"
+	"github.com/Azure/azure-sdk-for-go/sdk/to"
+)
+
+func TestLoadBalancer(t *testing.T) {
+	groupName := config.GenerateGroupName("network")
+	config.SetGroupName(groupName)
+
+	loadBalancerName := config.AppendRandomSuffix("loadbalancer")
+	publicIpAddressName := config.AppendRandomSuffix("pipaddress")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 300*time.Second)
+	defer cancel()
+	defer resources.Cleanup(ctx)
+
+	_, err := resources.CreateGroup(ctx, groupName)
+	if err != nil {
+		t.Fatalf("failed to create group: %+v", err)
+	}
+
+	publicIPAddress := armnetwork.PublicIPAddress{
+		Resource: armnetwork.Resource{
+			Location: to.StringPtr(config.Location()),
+		},
+
+		Properties: &armnetwork.PublicIPAddressPropertiesFormat{
+			IDleTimeoutInMinutes:     to.Int32Ptr(10),
+			PublicIPAddressVersion:   armnetwork.IPVersionIPv4.ToPtr(),
+			PublicIPAllocationMethod: armnetwork.IPAllocationMethodStatic.ToPtr(),
+		},
+		SKU: &armnetwork.PublicIPAddressSKU{
+			Name: armnetwork.PublicIPAddressSKUNameStandard.ToPtr(),
+		},
+	}
+	err = CreatePublicIPAddress(ctx, publicIpAddressName, publicIPAddress)
+	if err != nil {
+		t.Fatalf("failed to create public ip address: %+v", err)
+	}
+
+	err = CreateLoadBalancer(ctx, loadBalancerName, publicIpAddressName)
+	if err != nil {
+		t.Fatalf("failed to create load balancer: % +v", err)
+	}
+	t.Logf("created load balancer")
+
+	err = CreateInboundNatRule(ctx, loadBalancerName, inboundNatruleName)
+	if err != nil {
+		t.Fatalf("failed to get load balancer inbound nat rule: %+v", err)
+	}
+
+	err = GetLoadBalancerFrontendIPConfiguration(ctx, loadBalancerName, frontendIpConfigurationName)
+	if err != nil {
+		t.Fatalf("failed to get load balancer frontend IP configuration: %+v", err)
+	}
+
+	err = GetLoadBalancerBackendAddressPool(ctx, loadBalancerName, backendAddressPoolName)
+	if err != nil {
+		t.Fatalf("failed to get load balancer backend address pool: %+v", err)
+	}
+
+	err = GetLoadBalancerLoadBalancingRule(ctx, loadBalancerName, loadBalancingRuleName)
+	if err != nil {
+		t.Fatalf("failed to get the specified load balancer load balancing rule: %+v", err)
+	}
+	t.Logf("got the specified load balancer load balancing rule")
+
+	err = GetInboundNatRule(ctx, loadBalancerName, inboundNatruleName)
+	if err != nil {
+		t.Fatalf("failed to list the specified load balancer inbound nat rule: %+v", err)
+	}
+	t.Logf("listed the specified load balancer inbound nat rule")
+
+	err = GetLoadBalancerOutboundRule(ctx, loadBalancerName, outBoundRuleName)
+	if err != nil {
+		t.Fatalf("failed to get the specified load balancer outbound rule.: %+v", err)
+	}
+	t.Logf("got the specified load balancer outbound rule.")
+
+	err = ListLoadBalancerFrontendIPConfiguration(ctx, loadBalancerName)
+	if err != nil {
+		t.Fatalf("failed to get all the load balancer frontend IP configurations: %+v", err)
+	}
+	t.Logf("listed all the load balancer frontend IP configurations.")
+
+	err = GetLoadBalancerProbe(ctx, loadBalancerName, probeName)
+	if err != nil {
+		t.Fatalf("failed to get load balancer probe: %+v", err)
+	}
+	t.Logf("got load balancer probe")
+
+	err = ListLoadBalancerBackendAddressPool(ctx, loadBalancerName)
+	if err != nil {
+		t.Fatalf("failed to get all the load balancer backed address pools: %+v", err)
+	}
+	t.Logf("listed all the load balancer backed address pools")
+
+	err = ListLoadBalancerLoadBalancingRule(ctx, loadBalancerName)
+	if err != nil {
+		t.Fatalf("failed to list all the load balancing rules in a load balancer: %+v", err)
+	}
+	t.Logf("listed all the load balancing rules in a load balancer")
+
+	err = ListLoadBalancerNetworkInterface(ctx, loadBalancerName)
+	if err != nil {
+		t.Fatalf("failed to list associated load balancer network interfaces: %+v", err)
+	}
+	t.Logf("listed associated load balancer network interfaces")
+
+	err = ListLoadBalancerNetworkInterface(ctx, loadBalancerName)
+	if err != nil {
+		t.Fatalf("failed to list associated load balancer network interfaces: %+v", err)
+	}
+	t.Logf("listed associated load balancer network interfaces")
+
+	err = ListInboundNatRule(ctx, loadBalancerName)
+	if err != nil {
+		t.Fatalf("failed to list all the inbound nat rules in a load balancer: %+v", err)
+	}
+	t.Logf("listed all the inbound nat rules in a load balancer")
+
+	err = ListLoadBalancerOutboundRule(ctx, loadBalancerName)
+	if err != nil {
+		t.Fatalf("failed to list all the outbound rules in a load balancer: %+v", err)
+	}
+	t.Logf("listed all the outbound rules in a load balancer")
+
+	err = ListLoadBalancerProbe(ctx, loadBalancerName)
+	if err != nil {
+		t.Fatalf("failed to list load balancer probe: %+v", err)
+	}
+	t.Logf("listed load balancer probe")
+
+	err = GetLoadBalancer(ctx, loadBalancerName)
+	if err != nil {
+		t.Fatalf("failed to get the specified load balancer in a specified resource group: %+v", err)
+	}
+	t.Logf("got the specified load balancer in a specified resource group")
+
+	err = ListLoadBalancer(ctx)
+	if err != nil {
+		t.Fatalf("failed to list all the load balancers in a resource group: %+v", err)
+	}
+	t.Logf("listed all the load balancers in a resource group")
+
+	err = ListAllLoadBalancer(ctx)
+	if err != nil {
+		t.Fatalf("failed to list all the load balancer in a subscription: %+v", err)
+	}
+	t.Logf("listed all the load balancer in a subscription")
+
+	err = UpdateLoadBalancerTags(ctx, loadBalancerName)
+	if err != nil {
+		t.Fatalf("failed to update tags for load balancer: %+v", err)
+	}
+	t.Logf("updated load balancer tags")
+
+	err = DeleteInboundNatRule(ctx, loadBalancerName, inboundNatruleName)
+	if err != nil {
+		t.Fatalf("failed to delete the specified load balancer inbound nat rule: %+v", err)
+	}
+	t.Logf("deleted the specified load balancer inbound nat rule")
+
+	err = DeleteLoadBalancer(ctx, loadBalancerName)
+	if err != nil {
+		t.Fatalf("failed to delete load balancer: %+v", err)
+	}
+	t.Logf("deleted load balancer")
+}
