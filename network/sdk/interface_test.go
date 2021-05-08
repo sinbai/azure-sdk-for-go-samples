e43409f1972174d1f35c00aa800b4c609bd0d113
elena <sinbai@hotmail.com>
2021-05-08 15:22:55 +0800

    add interface test case

--- network/sdk/interface_test.go
+++ network/sdk/interface_test.go
@@ -16,7 +16,7 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/to"
 )
 
-func TestNetworkInterface(t *testing.T) {
+func TestInterface(t *testing.T) {
 	groupName := config.GenerateGroupName("network")
 	config.SetGroupName(groupName)
 	config.SetLocation("eastus")
